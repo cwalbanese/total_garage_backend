9145ec5ccafae9adba15b2e11e8f6915af42c624
cwalbanese <631gti@gmail.com>
2020-03-09 12:47:39 -0400

    setting anonymous user

--- total_garage/settings.py
+++ total_garage/settings.py
@@ -36,7 +36,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
