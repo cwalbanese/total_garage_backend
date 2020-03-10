7c7eeda74c5e37f37bff0ad2dcbafbed06d7df72
cwalbanese <631gti@gmail.com>
2020-03-10 13:30:28 -0400

    added middleware

--- total_garage/settings.py
+++ total_garage/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
 ]
 
 ROOT_URLCONF = 'total_garage.urls'
